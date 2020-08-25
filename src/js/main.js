83a809e6fcded35a2b6b28b28a143703756d225e
Alex <sasha-a@ukr.net>
2020-08-25 18:11:29 +0300

    Add mask to all phone inputs in forms and check input language in all inputs whith name - name and message

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInput from './modules/checkTextInput';
 
 window.addEventListener('DOMContentLoaded', () => {
     
@@ -10,6 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
-
+    mask('[name="phone"]');
+    checkTextInput('[name="name"]');
+    checkTextInput('[name="message"]');
 });
 
