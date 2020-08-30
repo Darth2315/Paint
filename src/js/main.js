5b510a9a63a88d819a633169cff8c0182a63af13
Alex <sasha-a@ukr.net>
2020-08-30 20:43:37 +0300

    Replace image when mouseover and mouseout on pictures

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import changeCalcState from './modules/changeCalcState';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
     
@@ -25,4 +26,5 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
 });
