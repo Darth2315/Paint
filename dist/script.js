9c94d993b206f0a9eb5b7c89a29fc1f793d39963
Alex <sasha-a@ukr.net>
2020-09-02 23:36:27 +0300

    Smooth scroll up is added to project.

--- dist/script.js
+++ dist/script.js
@@ -5127,12 +5127,59 @@ var scrolling = function scrolling(upSelector) {
     if (document.documentElement.scrollTop > 1650) {
       upElem.classList.add('animated', 'fadeIn');
       upElem.classList.remove('fadeOut');
-      console.log('HIII');
     } else {
       upElem.classList.add('fadeOut');
       upElem.classList.remove('fadeIn');
     }
   });
+  var element = document.documentElement,
+      body = document.body;
+
+  var calcScroll = function calcScroll() {
+    upElem.addEventListener('click', function (event) {
+      var scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+      if (this.hash !== '') {
+        event.preventDefault();
+        var hashElement = document.querySelector(this.hash),
+            hashElementTop = 0;
+
+        while (hashElement.offsetParent) {
+          hashElementTop += hashElement.offsetTop;
+          hashElement = hashElement.offsetParent;
+        }
+
+        hashElementTop = Math.round(hashElementTop);
+        smoothScroll(scrollTop, hashElementTop, this.hash);
+      }
+    });
+  };
+
+  var smoothScroll = function smoothScroll(from, to, hash) {
+    var timeInterval = 1,
+        prevScrollTop,
+        speed;
+
+    if (to > from) {
+      speed = 30;
+    } else {
+      speed = -30;
+    }
+
+    var move = setInterval(function () {
+      var scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+      if (prevScrollTop === scrollTop || to > from && scrollTop >= to || to < from && scrollTop <= to) {
+        clearInterval(move); // history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+      } else {
+        body.scrollTop += speed;
+        element.scrollTop += speed;
+        prevScrollTop = scrollTop;
+      }
+    }, timeInterval);
+  };
+
+  calcScroll();
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (scrolling);
