01ca7579b3d67316bcd4a9a2a1fe5f908ffcec93
Alex <sasha-a@ukr.net>
2020-09-03 23:52:30 +0300

    Started working on smooth scroll with requestAnimationFrame

--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -11,61 +11,89 @@ const scrolling = (upSelector) => {
         }
     });
 
-    const element = document.documentElement,
-          body = document.body;
+    // Smooth scrolling with requestAnimationFrame
 
-    const calcScroll = () => {
-        upElem.addEventListener('click', function(event) {
+    const links = document.querySelectorAll('[href^="#"]'),
+          speed = 0.7;
 
-            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    
+    links.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
 
-            if (this.hash !== '') {
-                event.preventDefault();
-                
-                let hashElement = document.querySelector(this.hash),
-                    hashElementTop = 0;
+        let widthTop = document.documentElement.scrollTop,
+            hash = this.hash,
+            toBlock = document.querySelector(hash),
+            start = null;
 
-                while (hashElement.offsetParent) {
-                    hashElementTop += hashElement.offsetTop;
-                    hashElement = hashElement.offsetParent;
-                }
-                
-                hashElementTop = Math.round(hashElementTop);
-                smoothScroll(scrollTop, hashElementTop, this.hash);
+        requestAnimationFrame(step);
+
+        function step(time) {
+            if (start === null) {
+                start = time;
             }
-        });
-    };
-
-    const smoothScroll = (from, to, hash) => {
-        let timeInterval = 1,
-            prevScrollTop,
-            speed;
-
-        if (to > from) {
-            speed = 30;
-        } else {
-            speed = -30;
         }
+        
+        });
+    });
 
-        let move = setInterval(function() {
-            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    // Pure js smooth scroll
 
-            if (
-                prevScrollTop === scrollTop ||
-                (to > from && scrollTop >= to) ||
-                (to < from && scrollTop <= to)
-            ) {
-                clearInterval(move);
-                // history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
-            } else {
-                body.scrollTop += speed;
-                element.scrollTop += speed;
-                prevScrollTop = scrollTop;
-            }
-        }, timeInterval);
-    };
+    // const element = document.documentElement,
+    //       body = document.body;
 
-    calcScroll();
+    // const calcScroll = () => {
+    //     upElem.addEventListener('click', function(event) {
+
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+    //         if (this.hash !== '') {
+    //             event.preventDefault();
+                
+    //             let hashElement = document.querySelector(this.hash),
+    //                 hashElementTop = 0;
+
+    //             while (hashElement.offsetParent) {
+    //                 hashElementTop += hashElement.offsetTop;
+    //                 hashElement = hashElement.offsetParent;
+    //             }
+                
+    //             hashElementTop = Math.round(hashElementTop);
+    //             smoothScroll(scrollTop, hashElementTop, this.hash);
+    //         }
+    //     });
+    // };
+
+    // const smoothScroll = (from, to, hash) => {
+    //     let timeInterval = 1,
+    //         prevScrollTop,
+    //         speed;
+
+    //     if (to > from) {
+    //         speed = 30;
+    //     } else {
+    //         speed = -30;
+    //     }
+
+    //     let move = setInterval(function() {
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+    //         if (
+    //             prevScrollTop === scrollTop ||
+    //             (to > from && scrollTop >= to) ||
+    //             (to < from && scrollTop <= to)
+    //         ) {
+    //             clearInterval(move);
+    //             // history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+    //         } else {
+    //             body.scrollTop += speed;
+    //             element.scrollTop += speed;
+    //             prevScrollTop = scrollTop;
+    //         }
+    //     }, timeInterval);
+    // };
+
+    // calcScroll();
 };
 
 export default scrolling;
